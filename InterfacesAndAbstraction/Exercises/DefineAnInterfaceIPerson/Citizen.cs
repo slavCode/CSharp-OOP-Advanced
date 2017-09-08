43af3027ba9ccb751e388f1138493bb43ba2d54a
Slav Tomov <stomow@gmail.com>
2017-09-08 11:17:58 +0300

    Interfaces And Abstraction Exercises

---
+++ InterfacesAndAbstraction/Exercises/DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,11 @@
+﻿public class Citizen : IPerson
+{
+    public string Name { get; }
+    public int Age { get; }
+
+    public Citizen(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
---
+++ InterfacesAndAbstraction/Exercises/DefineAnInterfaceIPerson/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+public class StartUp
+{
+    public static void Main()
+    {
+            Type personInterface = typeof(Citizen).GetInterface("IPerson");
+            PropertyInfo[] properties = personInterface.GetProperties();
+            Console.WriteLine(properties.Length);
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            IPerson person = new Citizen(name, age);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+    }
+}
