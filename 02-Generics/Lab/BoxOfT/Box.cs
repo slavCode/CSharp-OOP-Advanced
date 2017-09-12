d291a1813c25ce35313b2817bfe080cfa2046671
Slav Tomov <stomow@gmail.com>
2017-09-12 16:13:01 +0300

    Generics Lab

---
+++ 02-Generics/Lab/BoxOfT/Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Box<T>
+{
+    private List<T> data;
+
+    public Box()
+    {
+        this.data = new List<T>();
+    }
+
+    public void Add(T element)
+    {
+        data.Add(element);
+    }
+
+    public int Count => this.data.Count;
+
+    public T Remove()
+    {
+        var remove = this.data.Last();
+        this.data.RemoveAt(this.data.Count -1);
+        return remove;
+    } 
+}
